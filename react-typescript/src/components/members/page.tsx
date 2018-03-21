68ea9f1e9e2c62d10ffe8d320342fb3ab95813d9
Huzhangjie <1534634433@qq.com>
2018-03-21 23:21:56 +0800

    子组件 member page组件 在header组件中link转路由映射显示

---
+++ react-typescript/src/components/members/page.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+export const MembersPage: React.StatelessComponent<{}> = () => {
+  return (
+    <div className="row">
+      <h2>Members page</h2>
+    </div>
+  )
+}
